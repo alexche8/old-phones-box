0b4bb5712b193634236bf6ebbea79b036cb3aa3c
Alexandr Chervyak <alexche8@gmail.com>
2018-02-26 09:31:10 +0200

    Add text funciton

--- nokia130.sh
+++ nokia130.sh
@@ -21,8 +21,23 @@ mk_imageaudio(){
     echo $(readlink -f "$file_name" )
 }
 
+mk_textedaudio(){
+    echo "hello"
+
+    ffmpeg -i "$1" -vf drawtext="/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf: \
+text='Stack Overflow': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: \
+boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -codec:a copy output.mp4
+
+ffmpeg  -i "$1" -f lavfi -i color=c=blue:s=160x128:d=0.5 -vf \
+"drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf:fontsize=30: \
+ fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='Stack Overflow'" \
+-codec:a copy output.mp4
+}
+
 if [ "$1" = "mk_imageaudio" ];then
     mk_imageaudio "$2" "$3" "$4"
+elif [ "$1" = "mk_textedaudio" ];then
+    mk_textedaudio "$2" "$3"
 else
     echo "Command '$1' not supported"
 fi
