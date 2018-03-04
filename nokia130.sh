e187c7fa4b3b847825187915a267d47849169209
Alexandr Chervyak <alexche8@gmail.com>
2018-03-04 21:56:01 +0200

    Refactor

--- nokia130.sh
+++ nokia130.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-opb_images=$HOME/.opb_images
-
 # 1. Make imageaudio, textedaudio, blackscreenaudio
-# 2. Fix tags for mp3 file to make right order DONE
 # 4. Copy contact book.
-# 5. Fix tags for mp3 file to make right order set order:song to title if not works
+# 6. Download youtube video and playlists
 
 mk_imageaudio(){
-    if [ -z "$3" ]; then
-        file_name="${1%.*}"_nokia130.mp4
-    else
-        file_name=$3
-    fi
-
-    #if [ -z "$2" ]; then
-    #    image=$opb_images/default.jpg
-    #    image=${2:-$opb_images/default.jpg}
-    #else
-    #    image=$2
-    #fi
+    file_name="${1%.*}"_nokia130.mp4
 
     convert -size 32x32 xc:white default.jpg
 
@@ -29,11 +15,17 @@ mk_imageaudio(){
     echo $(readlink -f "$file_name" )
 }
 
-format_tracks_titles(){
+format_title_tag(){
     export PYTHONIOENCODING=utf8
     new_title=$(eyeD3 -P display --pattern "%track%.%title%" "$1")
     echo $new_title
     eyeD3 --title="$new_title" "$1"
+}
+
+format_title_tag_dir(){
+    for i in *.mp3; do
+        format_title_tag "$i"
+    done;
 }
 
 
@@ -41,14 +33,20 @@ format_album_name(){
     album=$(eyeD3 -P nfo "$1" | grep Album | cut -d: -f2)
     artist=$(eyeD3 -P nfo "$1" | grep Artist | cut -d: -f2)
     year=$(eyeD3 -P nfo "$1" | grep Recorded | cut -d: -f2)
-    mv "$1" "${artist:1}[${year:1}]-${album:1}"
-    echo -e "$1" formated as "${artist:1}[${year:1}]-${album:1}"
+    mv "$1" "${artist:1}${year:1}-${album:1}"
+    echo -e "$1" formated as "${artist:1}${year:1}-${album:1}"
+}
+
+format_albums_dir(){
+    find . -type d -exec nokia130 format_album_name {} \;
 }
 
 if [ "$1" = "mk_imageaudio" ];then
-    mk_imageaudio "$2" "$3" "$4"
+    mk_imageaudio "$2"
 elif [ "$1" = "format_tracks_titles" ];then
     format_tracks_titles "$2"
+elif [ "$1" = "format_title_tag_dir" ];then
+    format_title_tag_dir"$2"
 elif [ "$1" = "format_album_name" ];then
     format_album_name "$2"
 else
