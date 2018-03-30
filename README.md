14ffa0262320adb0591bc48c48c50953a96a328c
Alexandr Chervyak <alexche8@gmail.com>
2018-03-31 00:06:09 +0300

    Remove deprecated command, add common section to readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # old-phones-box
 Utils.
+
+Common moments.
+---------------
+
+Many flash devices shufle files during the recording. In case of mp3 albums/folders that means that user
+has not ability to listen music in right order. It can be uncomfortable. For audiobooks is impossible.
+
+To fix that problem on linux, use fatsort utility:
+
+1. Install(Ubuntu):
+
+sudo apt-get install fatsort
+
+2. Format:
+
+https://askubuntu.com/a/358533/547576
--- nokia130.sh
+++ nokia130.sh
@@ -9,6 +9,11 @@ convert_video(){
     ffmpeg -i "$1" -c:v mpeg4 -q:v 3 -c:a mp3 -vf scale=160:-1 "$output"
 }
 
+convert_video2(){
+    output="${1%.*}"_transcend.amv
+    ffmpeg -i "$1" -c:v amv -q:v 3 -c:a mp3 -vf scale=160:-1 "$output"
+}
+
 pull_youtube_playlist(){
     youtube-dl -o "%(title)s.%(ext)s" "$1"
 }
@@ -32,22 +37,24 @@ format_album_name(){
     echo -e "$1" formated as "${artist:1}${year:1}-${album:1}"
 }
 
-copy_dir(){
-    for dir in */; do
-        cp -r --verbose "$dir"* "$2"/"$dir"
-    done
+format_albums_names(){
+    for i in *; do
+        format_album_name "$i"
+    done;
 }
 
 if [ "$1" = "mk_imageaudio" ];then
     mk_imageaudio "$2"
 elif [ "$1" = "format_album_name" ];then
     format_album_name "$2"
-elif [ "$1" = "copy_dir" ];then
-    copy_dir "$2" "$3"
+elif [ "$1" = "format_albums_names" ];then
+    format_albums_names "$2"
 elif [ "$1" = "pull_youtube_playlist" ];then
     pull_youtube_playlist "$2"
 elif [ "$1" = "convert_video" ];then
     convert_video "$2"
+elif [ "$1" = "convert_video2" ];then
+    convert_video2 "$2"
 else
     echo "Command '$1' not supported"
 fi
